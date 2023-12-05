455a1e6b7438e6c4551d12e9085a27b1ade181c6
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2023-12-05 20:34:30 +0000

    Added podman deployment script

---
+++ push_to_ecr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Source the Python script to get the values
+source ./app.py
+
+# AWS Configuration
+AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+
+# Set your AWS configuration
+AWS_REGION="us-east-1"
+AWS_ACCOUNT_ID="690469543125"
+ECR_REPOSITORY="etl-project-container-repository"
+
+# Authenticate with ECR
+echo "Authenticating with ECR..."
+aws ecr get-login-password --region $AWS_REGION | podman login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+# Tag your local Podman image with the ECR repository URI
+echo "Tagging the local image..."
+podman tag aws_etl_project_image:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:latest
+
+# Push the image to ECR using the AWS access key and secret key
+echo "Pushing the image to ECR..."
+podman push --creds=$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:latest
+
+echo "Image successfully pushed to ECR."
