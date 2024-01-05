8768886b7436ca67a0e8c7d3f4977d670f6d2b3a
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2024-01-05 14:54:08 +0000

    Developed results modal

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-import pandas as pd
-import os
-from flask import Flask, render_template, request, make_response, redirect, url_for
-import joblib
-from datetime import datetime
-from werkzeug.utils import secure_filename
 import logging
-from logging.handlers import RotatingFileHandler
+import os
 import boto3
-from io import StringIO
-from io import BytesIO
+import joblib
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import io
+import base64
+
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+from io import BytesIO, StringIO
+from flask import (Flask, make_response, redirect, render_template, request,
+                   url_for)
+from scipy.stats import norm, percentileofscore
+from werkzeug.utils import secure_filename
+
 from env import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 
@@ -174,15 +182,77 @@ class EtlProjectApp(Flask):
 
         return {'results': []}
 
+    def plot_score(self, score):
+        matplotlib.use('Agg')
+
+        # Set the mean and standard deviation for the normal distribution
+        mean = 0
+        std_dev = 1
+        data_points = 100
+
+        # Generate random numbers from a normal distribution
+        data = np.random.normal(mean, std_dev, data_points)
+
+        # Fit a normal distribution to the data
+        fit_mean, fit_std = norm.fit(data)
+
+        bins = max(10, round(data_points / 20))
+
+        # Plot the histogram of the random numbers
+        plt.hist(data, bins=bins, density=True, alpha=0.5, color='#007bff')
+
+        # Overlay a normal distribution curve
+        xmin, xmax = plt.xlim()
+        x = np.linspace(xmin, xmax, 100)
+        p = norm.pdf(x, fit_mean, fit_std)
+        plt.plot(x, p, 'k', linewidth=2)
+
+        # Add vertical lines to represent percentiles
+        percentiles = [25, 50, 75]
+        percentile_values = np.percentile(data, percentiles)
+        for percentile, value in zip(percentiles, percentile_values):
+            plt.axvline(value, color='black', linestyle='dashed', linewidth=1)
+
+        # Add a vertical line to represent a specific value (e.g., x=2)
+        plt.axvline(score, color='#007bff', linestyle='solid',
+                    linewidth=2, label=f'Your score')
+
+        # Calculate and display the percentile of the specific value
+        score_percentile = percentileofscore(data, score)
+        plt.title(f'Score Distribution')
+
+        # Add labels and title
+        plt.xlabel('Value')
+        plt.ylabel('Probability Density')
+
+        # Add legend
+        plt.legend()
+
+        # Save the plot as a PNG in memory
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+
+        # Convert the image data to a base64-encoded string
+        image_string = base64.b64encode(buffer.read()).decode('utf-8')
+
+        # Close the buffer to free up resources
+        buffer.close()
+
+        plt.clf()
+
+        return image_string, score_percentile
+
     def predict(self):
         # Shortened name for self.uploads_data_frames
         meta_df = self.meta_data_frame
 
         # Extract input data from the POST request
-        lead = float(request.form.get('lead'))
-        director = float(request.form.get('director'))
-        genre = float(request.form.get('genre'))
-        budget = float(request.form.get('budget'))
+        data = request.get_json()
+        lead = float(data.get('lead'))
+        director = float(data.get('director'))
+        genre = float(data.get('genre'))
+        budget = float(data.get('budget'))
 
         # Calculate artificial features
         matching_rows = meta_df[(meta_df['lead'] == lead)]
@@ -219,10 +289,17 @@ class EtlProjectApp(Flask):
 
         # Make predictions using the decision tree model
         profit_ratio_prediction = self.decision_tree_model.predict(input)
+
+        image_string, score_percentile = self.plot_score(
+            profit_ratio_prediction)
+
         profit = round((profit_ratio_prediction[0] * budget) - budget)
 
-        # Redirect to the index route with profit as a query parameter
-        return redirect(url_for('index', profit=profit))
+        return {
+            'profit': "${:,.2f}".format(profit),
+            'score_percentile': tuple(score_percentile)[0],
+            'image_string': image_string
+        }
 
     def catch_all_route(self, catch_all):
         return render_template('main.html')
